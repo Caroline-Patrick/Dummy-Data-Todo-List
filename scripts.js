430bd87c9a7a9c6b8b4e114fa6d052310b94174a
Caroline-Patrick <carolinespatrick@gmail.com>
2022-08-04 14:35:35 -0500

    added comments with explanation of code

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-    // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
 
     const arrayOfTodos1 = [
         {
@@ -14,45 +13,44 @@
         "completed": false
     }]
 
-    
+    //the onclick of fetchtodos - requests data from url listed. Next it goes into the '.then' promise (b/c can take awhile, needs ordered). the 'anonymous' function w/ argument of the 'response' variable - which we named- it's just the raw data from URL. the anonymous function called transforms the raw data into JS object notation, which we named 'd'. Then we call another anonymous function? that calls populateTOdos function w/ arugment of 'd'.
     const fetchTodos = () => {
         fetch('https://reqres.in/api/users?page=2')
         .then( (response) => response.json()) 
         .then( (d) => populateTodos(d))
-    
-    }
-    
-    const logTodos = () => {
-        console.log(arrayOfTodos1)
+
 
     }
+    const logTodos = () => {
+        console.log(arrayOfTodos1)
+        //this function is just logging out (in the console) the 2 object list above when you click 'log to dos' button on site
+    }
     
-    const populateTodos = (d) => {
+    const populateTodos = (d) => { //defining function populateTodos and inputting the argument of the JSON-styled data, 'd'
        
-        const todoList = document.getElementById("todo-list")
+       
+        const todoList = document.getElementById("todo-list") //the ordered list (ol) element becomes the variable - 'todoList'
 
 
 
         for (let i = 0; i < d.data.length; i++) {
 
-            const element = document.createElement("li")
-            element.innerHTML = `<img src="${d.data[i].avatar}">`
-            todoList.appendChild(element)
+           
+            const element = document.createElement("li")  //creating variable, "element", which stands for the 'li' element we are creating in the document - with createElement method
+
+            element.innerHTML = `<img src="${d.data[i].avatar}">` // these are directions to go into the created 'li' element (variable name =element) and add to it some HTML code. Specifically, we are adding it as an image. The image we are adding is the 'avatar' key from each JSON object. 
+
+            //d.data[i].avatar - "d" is JSON-styled data. "data" is an array within "d" we are accessing (array of  6 objects, indexed 0-5), [i] stands for the object w/in 'data' array we are accessing, and then 'avatar' is one of the keys we are targeting on the object (in this case it's an image)
+           
+           
+            
+            todoList.appendChild(element)// Takes the ordered list element (that we've made into a variable) and uses the appendChild method to add the 'li' element we created w/ the d.data[i].avatar information into the html itself, so that the images populate as a numbered list when 'fetch to dos' is clicked
+
+            console.log (d) // logging out JSON styled data, so I could see what this line of code (d.data[i].avatar ) was saying
+
+
+
         
     }
     }
-    //Caroline's code from here
-
-//     console.log(`Title: ${arrayOfTodos1[0].title}`)
-
-//     let list = document.getElementbyID("todo-list");
-//     let listItemContent = arrayOfTodos[0].title;
-
-//   let createListItem = () => {
-    // let newObject = document.createElement("LI");
-    // let text = document.createTextNode(listItemContent);
-
-//     newObject.appendChild(text);
-
-//     document.getElementById("todo-list").appendChild(newObject)
-//    }
+    
