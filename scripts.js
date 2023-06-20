e349feadc80a9cc225b57df9911f47dba26b499d
Caroline-Patrick <carolinespatrick@gmail.com>
2023-06-20 13:54:09 -0500

    cleaned up and added incomplete todos filter function

--- scripts.js
+++ scripts.js
@@ -1,25 +1,3 @@
-
-    // const arrayOfTodos1 = [
-    //     {
-    //     "userId": 14,
-    //     "id": 1,
-    //     "title": "delectus aut autem",
-    //     "completed": false
-    // },
-    // {
-    //     "userId": 20,
-    //     "id": 2,
-    //     "title": "delectus aut autem",
-    //     "completed": false
-    // }]
-
-    //the onclick of fetchtodos - requests data from url listed. Next it goes into the '.then' promise (b/c can take awhile, needs ordered). the 'anonymous' function w/ argument of the 'response' variable - which we named- it's just the raw data from URL. the anonymous function called transforms the raw data into JS object notation, which we named 'd'. Then we call another anonymous function? that calls populateTOdos function w/ arugment of 'd'.
-    // const fetchTodos = () => {
-    //     fetch('https://reqres.in/api/users?page=2')
-    //     .then( (response) => response.json()) 
-    //     .then( (d) => populateTodos(d)) }
-
- 
     const fetchTodos = () => {
           
         fetch('https://jsonplaceholder.typicode.com/todos')
@@ -29,56 +7,32 @@
                
               
             }
-     //fetches data from json, then takes that data and transforms into json format (and stores as d), then logs out the json format in console
-
-
-
-    // const logTodos = () => {
-    //     console.log(arrayOfTodos1)
-    //     //this function is just logging out (in the console) the 2 object list above when you click 'log to dos' button on site
-    // }
+     //fetches data from json, then takes that data and transforms into json format (and stores as d), then sets a new variable called arrayOfTodos to equal the value of d. 
 
     const logTodos = () => {
       console.log(arrayOfTodos)
     }
     
-    const populateTodos = () => { //defining function populateTodos and inputting the argument of the JSON-styled data, 'd'
-       
+    const populateTodos = () => { 
       
-        
-         //the ordered list (ol) element becomes the variable - 'todoList'
-
-
-
         for (let i = 0; i < arrayOfTodos.length; i++) {
 
-            let todoList = document.getElementById("todo-list")
+            let todoList = document.getElementById("todo-list") //the ordered list (ol) element becomes the variable - 'todoList'
            
             let element = document.createElement("li")  //creating variable, "element", which stands for the 'li' element we are creating in the document - with createElement method
 
-             let textNode = document.createTextNode(arrayOfTodos[i].userId) // these are directions to go into the created 'li' element (variable name =element) and add some HTML code to it. 
+             let textNode = document.createTextNode(arrayOfTodos[i].title) // these are directions to go into the created 'li' element (variable name =element) and add some HTML code to it. 
             element.appendChild(textNode)
-            // Specifically, we are adding it as an image. The image we are adding is the 'avatar' key from each JSON object. 
-
-            //d.data[i].avatar - "d" is JSON-styled data. "data" is an array within "d" we are accessing (array of  6 objects, indexed 0-5), [i] stands for the object w/in 'data' array we are accessing, and then 'avatar' is one of the keys we are targeting on the object (in this case it's an image)
-           
            
             
-            todoList.appendChild(element)// Takes the ordered list element (that we've made into a variable) and uses the appendChild method to add the 'li' element we created w/ the d.data[i].avatar information into the html itself, so that the images populate as a numbered list when 'fetch to dos' is clicked
-
-            // console.log (d) // logging out JSON styled data, so I could see what this line of code (d.data[i].avatar ) was saying
-
-
-
+            todoList.appendChild(element)// Takes the ordered list element (that we've made into a variable) and uses the appendChild method to add the 'li' element we created w/ the textNode information into the html itself, so that the todo items populate as a numbered list when 'fetch to dos' is clicked
         
     }
     }
 
-    // cocnst clearTodos = () => {
-    //   element = null
-    // }
+
     const clearToDos = () => {
-      document.getElementById("todo-list").innerHTML= null    
+      document.getElementById("todo-list").innerHTML= ''    
 
     } //keeping this button's only function for clearing
 
@@ -86,7 +40,7 @@
 
 
     let printFilter =() => {
-      document.getElementById("todo-list").innerHTML= null //clears the printed log again
+     clearToDos()
 
       let todoList = document.getElementById("todo-list")
       let num = document.getElementById("number-input").value
@@ -96,7 +50,7 @@
       for (let i = 0; i < filteredArray.length; i++) {
   
       let element = document.createElement("li")  //creating variable, "element", which 
-      element.innerHTML = filteredArray[i].userId
+      element.innerHTML = filteredArray[i].title
  
       todoList.appendChild(element)//
     }}
@@ -104,18 +58,37 @@
 
     
     let completedToDos = () => {
-      document.getElementById("todo-list").innerHTML= null //clears the printed log again
+      clearToDos()
 
-      let todoList = document.getElementById("todo-list")
-      let num = document.getElementById("number-input").value
+      let todoList = document.getElementById("todo-list") // OL list as a variable
+    
+      let num = document.getElementById("number-input").value // input value from user as a variable
+    
      
-      let filteredArray = arrayOfTodos.filter(array => array.userId == num)
+      let filteredArray = arrayOfTodos.filter(array => array.userId == num && array.completed) // filters array to just those objects that match user input #
 
       for (let i = 0; i < filteredArray.length; i++) {
-  
-      let element = document.createElement("li")  //creating variable, "element", which 
-      element.innerHTML = filteredArray[i].userId
+        let element = document.createElement("li")
+      //  element.innerHTML = filteredArray[i].userId 
+       element.innerHTML = filteredArray[i].title
  
       todoList.appendChild(element)
 
-    }}
+    }
+  }
+
+  let incompleteToDos = () => {
+    clearToDos();
+    let incompleteToDoList = document.getElementById("todo-list")
+    let num = document.getElementById("number-input").value
+
+    let filteredArray=arrayOfTodos.filter(array => array.userId == num && array.completed == false)
+
+    console.log(filteredArray);
+
+    for(let i=0; i< filteredArray.length; i++) {
+    let element = document.createElement("li") 
+    element.innerHTML = filteredArray[i].title
+    incompleteToDoList.appendChild(element)
+    }
+  }
